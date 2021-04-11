9b4fb4e555ab5b0c9e54791799ad217d9c5a92e5
StevenTheBearded <>
2021-04-11 17:41:07 -0400

    delete functionality

--- src/main.js
+++ src/main.js
@@ -15,15 +15,15 @@ var letsCookButton = document.querySelector('#letsCook')
 var addNewButton = document.querySelector('#addNew')
 var viewFavoritesButton = document.querySelector('#viewFavorites')
 
-var favoriteBtn = document.querySelector('#favorite') //query selector for button added to DOM through displayRecipe()
-var rightArticle = document.querySelector('.right') // query selector for parent of display-card
+var favoriteBtn = document.querySelector('#favorite')
+var rightArticle = document.querySelector('.right')
 
-var displayCard = document.querySelector('.display-card') // query selector for display-card
+var displayCard = document.querySelector('.display-card')
 
 var mainSection = document.querySelector('.main-section')
 var favoriteSection = document.querySelector('.favorite-section')
 
-var homeBtn = document.querySelector('#goHome') // FIX HOME BUTTON SWAP
+var homeBtn = document.querySelector('#goHome') 
 var favoriteCard = document.querySelector('.favorite')
 var articleFavorite = document.querySelector('#targetHere')
 
@@ -33,7 +33,6 @@ addRecipeButton.addEventListener('click', displayForm);
 addNewButton.addEventListener('click', addRecipe)
 displayCard.addEventListener('click', addFavorite)
 favoriteCard.addEventListener('click', goHome)
-// favoriteSection.addEventListener('click', deleteFavorite)
 
 //Event handlers
 window.addEventListener('load', pageLoad)
@@ -41,7 +40,6 @@ function pageLoad(){
 }
 
 function addFavorite(event){
-  // debugger
   if(!meal.favoritedRecipes.includes(recipe)){
     if(event.target.className === 'favoriteBtn') {
       meal.favoritedRecipes.push(recipe)
@@ -52,12 +50,6 @@ function addFavorite(event){
     display()
   }
 }
-/*
-  find index of specific click (create button to go with each index being printed to DOM to be deletable)
-   splice out at index
-  find way to append 'Add a Recipe' array
-*/
-
 
 function separateList(){
   var separateIndexes;
@@ -67,10 +59,6 @@ function separateList(){
   display(separateIndexes)
 }
 
-
-
-// onclick="remove(this)"
-//if event at click == 
 function display(){
   favoriteCard.innerHTML = " ";
   for(var i = 0; i < meal.favoritedRecipes.length;i++){
@@ -81,7 +69,7 @@ function display(){
   }
   favoriteCard.innerHTML += `<button class="go-home">Home</button>`
 }
-// onclick="remove(this)"
+
 function displayRecipe(recipe){
   event.preventDefault()
   displayCard.innerHTML = `
@@ -120,34 +108,17 @@ function createRecipe(){
     displayRecipe(recipe)
   }
 }
-/*
-how to target div? then once targeted,,,remove from array with splice?
-how to delete now that i have buttons set up? delete whole DIV and remove from 
-meals.favoriteRecipes array?
-linked through 'delete button' to activate/click the parent div. listener
-is only on the parent div. so when delete button is clicked, it takes out whole div.
-when delete button clicked, that event.target.className invokes another function
-to delelte WHOLE div to remove from DOM. 
-then shuld be able to go onto localstorage
 
-*/
 function goHome(event){
   if(event.target.className === 'go-home'){
     toggleDisplay()
   }
   if(event.target.className === 'deleteRecipe'){
-    //trigger event to target and remove <div> with classname of 'removeMe'
     meal.favoritedRecipes.indexOf(event)
     var index1 = meal.favoritedRecipes.indexOf(event)
     meal.favoritedRecipes.splice(index1, 1)
-    console.log(index1);
   }
 }
-
-// function deleteFavorite(event){
-//   var deleteIndex = meal.favoritedRecipes.indexOf(event.target.closest)
-//   console.log('clicked recipe name', deleteIndex)
-// }
 
 function displayForm(){
   footer.classList.toggle('hidden')
@@ -170,4 +141,3 @@ function remove(el) {
   var element = el;
   element.remove();
 }
-// onclick="remove(this)"
